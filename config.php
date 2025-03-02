1d5c2fa9af75276a4cf14f662e66ed88c1434842
sabri-jammoussi <sabrijm123@gmail.com>
2025-03-02 15:01:49 +0100

    config

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+class connexion
+{
+    public function CNXbase()
+    {
+        try {
+            $con = new PDO(
+                'mysql:host=localhost;dbname=projet_php_iit',
+                'root',
+                ''
+            );
+            return $con;
+            // $conn = mysqli_connect('localhost','root','','projet1');
+        } catch (Exception $e) {
+            echo 'Erreur de connexion: ' . $e->getMessage();
+        }
+    }
+}

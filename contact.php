5e599db34e095d9ed94e26d9c3541a11097ae071
sabri-jammoussi <sabrijm123@gmail.com>
2025-03-15 04:13:14 +0100

    adding the send email in the contact page

--- contact.php
+++ contact.php
@@ -1,3 +1,55 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+// Autoload PHPMailer classes if you're using Composer
+require 'vendor/autoload.php';
+
+// SMTP server configuration
+$mail = new PHPMailer(true);
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    try {
+        // Get the form data
+        $firstName = $_POST['c_fname'];
+        $lastName = $_POST['c_lname'];
+        $email = $_POST['c_email'];
+        $subject = $_POST['c_subject'];
+        $message = $_POST['c_message'];
+
+        // Set SMTP options
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com';  // Set the SMTP server to send through (use your provider's SMTP server)
+        $mail->SMTPAuth = true;
+        $mail->Username = 'sabrijm123@gmail.com'; // SMTP username
+        $mail->Password = 'ygcv oplu wjtp szmq'; // SMTP password
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->Port = 587;  // TCP port to connect to
+
+        // Recipients
+        $mail->setFrom($email, $firstName . ' ' . $lastName);
+        $mail->addAddress('sabrijm123@gmail.com', 'Admin'); // Add recipient email
+
+        // Content
+        $mail->isHTML(true);
+        $mail->Subject = $subject;
+        $mail->Body    = "<strong>Message from: </strong>" . $firstName . " " . $lastName . "<br>"
+            . "<strong>Email: </strong>" . $email . "<br>"
+            . "<strong>Message: </strong><br>" . nl2br($message);
+
+        // Send the email
+        if ($mail->send()) {
+            echo '<script>alert("Message has been sent successfully!");</script>';
+        } else {
+            echo '<script>alert("Message could not be sent. Mailer Error: ' . $mail->ErrorInfo . '");</script>';
+        }
+    } catch (Exception $e) {
+        echo '<script>alert("Message could not be sent. Mailer Error: ' . $mail->ErrorInfo . '");</script>';
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,30 +57,20 @@
     <title>Pharma &mdash; Colorlib Template</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
     <link href="https://fonts.googleapis.com/css?family=Rubik:400,700|Crimson+Text:400,400i" rel="stylesheet">
     <link rel="stylesheet" href="fonts/icomoon/style.css">
-
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/magnific-popup.css">
     <link rel="stylesheet" href="css/jquery-ui.css">
     <link rel="stylesheet" href="css/owl.carousel.min.css">
     <link rel="stylesheet" href="css/owl.theme.default.min.css">
-
-
     <link rel="stylesheet" href="css/aos.css">
-
     <link rel="stylesheet" href="css/style.css">
-
 </head>
 
 <body>
-
     <div class="site-wrap">
-
-
         <div class="site-navbar py-2">
-
             <div class="search-wrap">
                 <div class="container">
                     <a href="#" class="search-close js-search-close"><span class="icon-close2"></span></a>
@@ -37,7 +79,6 @@
                     </form>
                 </div>
             </div>
-
             <?php require_once "header.php"; ?>
         </div>
 
@@ -110,37 +151,6 @@
                 </div>
             </div>
         </div>
-
-
-
-        <div class="site-section bg-primary">
-            <div class="container">
-                <div class="row">
-                    <div class="col-12">
-                        <h2 class="text-white mb-4">Offices</h2>
-                    </div>
-                    <div class="col-lg-4">
-                        <div class="p-4 bg-white mb-3 rounded">
-                            <span class="d-block text-black h6 text-uppercase">New York</span>
-                            <p class="mb-0">203 Fake St. Mountain View, San Francisco, California, USA</p>
-                        </div>
-                    </div>
-                    <div class="col-lg-4">
-                        <div class="p-4 bg-white mb-3 rounded">
-                            <span class="d-block text-black h6 text-uppercase">London</span>
-                            <p class="mb-0">203 Fake St. Mountain View, San Francisco, California, USA</p>
-                        </div>
-                    </div>
-                    <div class="col-lg-4">
-                        <div class="p-4 bg-white mb-3 rounded">
-                            <span class="d-block text-black h6 text-uppercase">Canada</span>
-                            <p class="mb-0">203 Fake St. Mountain View, San Francisco, California, USA</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-        </div>
         <?php require_once "footer.php" ?>
     </div>
 
@@ -151,7 +161,6 @@
     <script src="js/owl.carousel.min.js"></script>
     <script src="js/jquery.magnific-popup.min.js"></script>
     <script src="js/aos.js"></script>
-
     <script src="js/main.js"></script>
 
 </body>

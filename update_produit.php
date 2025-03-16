f1bf3d92b4f7063b4982c7e94e5b81c0b40227c9
sabri-jammoussi <sabrijm123@gmail.com>
2025-03-16 02:45:53 +0100

    display the stock in all crud

--- update_produit.php
+++ update_produit.php
@@ -8,17 +8,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nom = $_POST['nom'];
     $description = $_POST['description'];
     $prix = $_POST['prix'];
+    $stock = $_POST['stock'];
 
     try {
         if (!empty($_FILES['image']['tmp_name'])) {
             echo "<script>console.log('Data received:');</script>";
 
             $imageData = file_get_contents($_FILES['image']['tmp_name']);
-            $stmt = $pdo->prepare("UPDATE produits SET nom=?, description=?, prix=?, image=? WHERE id=?");
-            $stmt->execute([$nom, $description, $prix, $imageData, $id]);
+            $stmt = $pdo->prepare("UPDATE produits SET nom=?, description=?, prix=?,stock=?, image=? WHERE id=?");
+            $stmt->execute([$nom, $description, $prix, $stock, $imageData, $id]);
         } else {
-            $stmt = $pdo->prepare("UPDATE produits SET nom=?, description=?, prix=? WHERE id=?");
-            $stmt->execute([$nom, $description, $prix, $id]);
+            $stmt = $pdo->prepare("UPDATE produits SET nom=?, description=?, prix=?,stock=? WHERE id=?");
+            $stmt->execute([$nom, $description, $prix, $stock, $id]);
         }
 
         header("Location: listeProduit.php");
